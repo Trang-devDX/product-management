18b0e558d3cc5b49901fc7b145c75a138e966c69
Admin <nguyenthanhtrang2733@gmail.com>
2024-07-21 10:39:42 +0700

    filter-status & search

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,24 +2,31 @@ const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
 
+const searchHelper = require('../../helpers/search');
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
     // bộ lọc
     const filterStatus = filterStatusHelper(req.query);
 
-    //tìm kiếm
     let find = {
         deleted: false,
     };
     if (req.query.status) {
         find.status = req.query.status;
     }
-    let keyword = "";
-    if (req.query.keyword) {
-        keyword = req.query.keyword;
-        const regex = new RegExp(keyword, "i");
-        find.title = regex;
+
+    //tìm kiếm
+    const objectSearch = searchHelper(req.query);
+    if(objectSearch.regex){
+        find.title = objectSearch.regex
     }
+    // let keyword = "";
+    // if (req.query.keyword) {
+    //     keyword = req.query.keyword;
+    //     const regex = new RegExp(keyword, "i");
+    //     find.title = regex;
+    // }
 
     const products = await Product.find(find);
 
@@ -27,6 +34,6 @@ module.exports.index = async (req, res) => {
         titlePage: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword,
+        keyword: objectSearch.keyword,
     });
 };
