1139e1e783d67f7bef43fe49d2e7cd8b0e784039
Admin <nguyenthanhtrang2733@gmail.com>
2024-07-22 20:12:59 +0700

    change status product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -51,3 +51,12 @@ module.exports.index = async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 };
+
+// [GET] /admin/products/change-status/:status/:id
+module.exports.changeStatus = async (req, res) => {
+    const status = req.params.status;
+    const id = req.params.id;
+    await Product.updateOne({ _id: id }, { status: status})
+
+    res.redirect("back")
+}
